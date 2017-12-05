b40af7559d0d6977bc283ca0a36acd45c697bb16
developeratdaguanyuan <developeratdaguanyuan@gmail.com>
2017-12-05 10:19:49 -0600

    2D List Iterator

---
+++ 2DListIterator.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+
+class Solution {
+    private Iterator<List<Integer>> rowIter;
+    private Iterator<Integer> colIter;
+
+    public Solution(List<List<Integer>> vecs) {
+        
+        rowIter = vecs.iterator();
+        colIter = null;
+    }
+
+    public int next() {
+        return colIter.next();
+    }
+
+    public boolean hasNext() {
+        while (rowIter.hasNext() && (colIter == null || !colIter.hasNext())) {
+            colIter = rowIter.next().iterator();
+        }
+        return colIter.hasNext();
+    }
+
+    public void remove() {
+        if (colIter != null) {
+            colIter.remove();
+        }
+    }
+
+    public final static void main(String[] args) {
+        List<List<Integer>> vectors = new ArrayList<>();
+        List<Integer> row1 = new ArrayList<>();
+        row1.add(1);
+        row1.add(2);
+        row1.add(3);
+        vectors.add(row1);
+        List<Integer> row2 = new ArrayList<>();
+        row1.add(4);
+        row1.add(5);
+        vectors.add(row2);
+
+        Solution solution = new Solution(vectors);
+
+        System.out.println(solution.hasNext());
+        System.out.println(solution.next());
+        solution.remove();
+        System.out.println(solution.hasNext());
+        System.out.println(solution.next());
+        solution.remove();
+        System.out.println(solution.hasNext());
+        System.out.println(solution.next());
+        solution.remove();
+        System.out.println(solution.hasNext());
+        System.out.println(solution.next());
+        solution.remove();
+        System.out.println(solution.hasNext());
+        System.out.println(solution.next());
+        solution.remove();
+        System.out.println(solution.hasNext());
+
+        for (int i = 0; i < vectors.size(); i++) {
+            for (int j = 0; j < vectors.get(i).size(); j++) {
+                System.out.print(vectors.get(i).get(j));
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
