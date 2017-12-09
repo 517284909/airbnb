07206f99b8e9c918925a4a5a318fb7ccf049e162
developeratdaguanyuan <developeratdaguanyuan@gmail.com>
2017-12-09 09:03:44 -0600

    Case Combination

--- caseCombination.java
+++ caseCombination.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 class CaseCombination {
+    // Recursion
     public List<String> combination(String text) {
         text = text.toLowerCase();
         List<String> results = new ArrayList<>();
@@ -19,10 +20,28 @@ class CaseCombination {
         combination(text, current + Character.toUpperCase(text.charAt(idx)), results);
     }
 
+    // Iteration
+    public List<String> combinationIter(String text) {
+        text = text.toLowerCase();
+        List<String> results = new ArrayList<>();
+        results.add(text);
+        for (int i = 0; i < text.length(); i++) {
+            int size = results.size();
+            for (int j = 0; j < size; j++) {
+                StringBuilder t = new StringBuilder(results.get(j));
+                t.setCharAt(i, Character.toUpperCase(results.get(j).charAt(i)));
+                results.add(t.toString());
+            }
+        }
+        return results;
+    }
+
     public final static void main(String[] args) {
         CaseCombination caseCombination = new CaseCombination();
         System.out.println(caseCombination.combination("abc"));
         System.out.println(caseCombination.combination(""));
+        System.out.println(caseCombination.combinationIter("abc"));
+        System.out.println(caseCombination.combinationIter(""));
     }
 }
 
