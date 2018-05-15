8038ab44f3d316769cd3bd988ab02feb02c896da
hitzhoudi <hitzhoudi@gmail.com>
2018-05-14 20:37:09 -0500

    file system + callback

--- fileSystem.java
+++ fileSystem.java
@@ -1,11 +1,19 @@
 import java.util.*;
 
+class MyRunnable implements Runnable {
+   public void run() {
+      System.out.println("MyRunnable running");
+   }
+}
+
 class FileSystem {
     Map<String, Integer> pathMap;
+    Map<String, Runnable> callbackMap;
 
     public FileSystem() {
         pathMap = new HashMap<>();
         pathMap.put("", 0);
+        callbackMap = new HashMap<>();
     }
 
     public boolean create(String path, int value) {
@@ -18,6 +26,15 @@ class FileSystem {
             return false;
         }
         pathMap.put(path, value);
+
+        String recurPath = path;
+        while (recurPath.length() > 0) {
+            if (callbackMap.containsKey(recurPath))
+                callbackMap.get(recurPath).run();
+            int lastIndex = recurPath.lastIndexOf("/");
+            recurPath = recurPath.substring(0, lastIndex);
+        }
+
         return true;
     }
 
@@ -33,10 +50,25 @@ class FileSystem {
         return pathMap.get(path);
     }
 
+    public boolean watch(String path, Runnable callback) {
+        if (!pathMap.containsKey(path)) return false;
+        callbackMap.put(path, callback);
+        return true;
+    }
+
+    public void print(String path) {
+        System.out.println("watch: " + path);
+        //return "h";
+    }
+
     public final static void main(String[] args) {
         FileSystem fs = new FileSystem();
         System.out.println(fs.create("/a", 3));
+        System.out.println(fs.watch("/a", new MyRunnable()));
         System.out.println(fs.get("/a"));
+        System.out.println(fs.create("/a/z", 4));
+        System.out.println(fs.watch("/a/z", new MyRunnable()));
+
         System.out.println(fs.create("/b/c", 3));
         System.out.println(fs.get("/b/c"));
     }
